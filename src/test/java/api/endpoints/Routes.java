8758319eb251fa807f8a4c4a5130f7265c014544
Choure123-hub <chourebs@gmail.com>
2023-11-23 09:47:54 +0530

    commit message

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -19,7 +19,10 @@ public class Routes {
 	public static String delete_url=base_url+"/user/{username}";
 	
 	//Store module
-	
+	public static String Post_url=base_url+"/store/order";
+	public static String Get_url=base_url+"/store/order/{orderId}";
+	public static String Updatae_url=base_url+"/store/order/{orderId}";
+	public static String Delete_url=base_url+"/store/order/{orderId}";
 	         //here you will create store module URL,s
 	
 	//pet module
