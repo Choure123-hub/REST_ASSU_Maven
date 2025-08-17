2e2c6b0b9a89baf1488169df15c2c600d4da4585
Choure123-hub <132046824+Choure123-hub@users.noreply.github.com>
2025-08-17 19:58:10 +0530

    Update javaDemo.java
    complted java program

--- src/test/java/Rest_assured_Practice/javaDemo.java
+++ src/test/java/Rest_assured_Practice/javaDemo.java
@@ -3,5 +3,11 @@ package Rest_assured_Practice;
 public class javaDemo {
 public static void main(String[] args) {
 	String org = "bhagvanta";
+	String rev=" ";
+	for (int i=org.length()-1;i>0;i--)
+		{
+			rev=rev+org.charAt(i);
+		}
+	System.out.println(rev);
 }
 }
